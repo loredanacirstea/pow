f9651169e048d39c38baf49db81d72269355011d
Loredana <loredana.cirstea@gmail.com>
2019-02-20 01:10:32 +0200

    Initial client structure

---
+++ client/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-content>
+        <PoPResource
+            :title=title
+        />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import PoPResource from './components/PoPResource.vue';
+
+export default {
+  name: 'app',
+  components: {
+    PoPResource,
+  },
+  data () {
+    return {
+        title: 'Clap whenever you like the presentation!',
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/PoPResource.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-container>
+        <v-layout row wrap>
+            <v-flex xs12>
+                <div class="subheading">{{title}}</div>
+            </v-flex>
+        </v-layout>
+        <v-layout row wrap>
+            <v-flex xs12>
+                <v-avatar class="v-btn v-btn--depressed theme--light v-btn--floating"
+                    :size="120"
+                    v-on:click.stop="clap()"
+                >
+                    <img src="/clap.svg" alt="avatar">
+                </v-avatar>
+            </v-flex>
+        </v-layout>
+        <v-layout row wrap>
+
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+
+export default {
+    props: ['title'],
+    components: {
+    },
+    methods: {
+        clap() {
+            console.log('clapped')
+        }
+    }
+}
+</script>
+
+<style>
+html, body {
+    margin: 0;
+}
+</style>
