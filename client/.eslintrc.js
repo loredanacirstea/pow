d9324c9cbf80f7da110c8a7adf0f5076a7589c79
Loredana <loredana.cirstea@gmail.com>
2019-02-22 13:10:53 +0200

    eslint config changes

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,17 +1,24 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/airbnb',
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'indent': ['error', 4],
+        'object-curly-spacing': ['error', 'never'],
+        'object-curly-newline': 'off',
+        'no-plusplus': 'off',
+        'func-names': ['error', 'never'],
+        'import/prefer-default-export': 'off',
+        'no-console': 'off',
+    },
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
 };
