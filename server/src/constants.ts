6998cb897884cd3dccff0bfd470721842f8b2e3f
Loredana <loredana.cirstea@gmail.com>
2019-02-22 15:20:34 +0200

    Server - add rest config constants

---
+++ server/src/constants.ts
@@ -0,0 +1,11 @@
+export const REST_CONFIG = {
+    apiExplorer: {
+        disabled: true,
+    },
+    cors: {
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST',
+    },
+    port: 3000,
+    host: '127.0.0.1',
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,19 @@
 import {PoPApplication} from './application';
 import {ApplicationConfig} from '@loopback/core';
+import {REST_CONFIG} from './constants';
 
 export {PoPApplication};
 
 export async function main(options: ApplicationConfig = {}) {
-  const app = new PoPApplication(options);
-  await app.boot();
-  await app.start();
+    const app = new PoPApplication({
+        rest: REST_CONFIG
+    });
+    await app.boot();
+    await app.start();
 
-  const url = app.restServer.url;
-  console.log(`Server is running at ${url}`);
-  console.log(`Try ${url}/ping`);
+    const url = app.restServer.url;
+    console.log(`Server is running at ${url}`);
+    console.log(`Try ${url}/ping`);
 
-  return app;
+    return app;
 }
